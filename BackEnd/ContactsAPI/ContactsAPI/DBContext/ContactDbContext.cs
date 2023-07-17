a03ab5969e6e95377d04a60d645edf4c77a02d8e
Marvin Meneses <Marvin.Meneses@wdc.com>
2023-07-17 13:45:48 +0800

    crud contact api

---
+++ BackEnd/ContactsAPI/ContactsAPI/DBContext/ContactDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ContactsAPI.Models;
+
+namespace ContactsAPI.DBContext
+{
+    public class ContactDbContext: DbContext
+    {
+        protected override void OnConfiguring
+       (DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase(databaseName: "ContactDb");
+        }
+        public DbSet<Contact> Contacts { get; set;}
+    }
+}
